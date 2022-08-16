b1a9b64de459e0a086620b759523ab32b6d8d619
kodoku169 <79566948+kodoku169@users.noreply.github.com>
2022-08-16 17:35:08 +0700

    search product js

--- blog/public/js/sidebar.js
+++ blog/public/js/sidebar.js
@@ -23,7 +23,17 @@ $(".type ").html(filter_loaihinh)
 
 
 $('.input').on('input', function () {
-    console.log($(this).val()); // lấy giá trị
+    // console.log($(this).val()); // lấy giá trị
     // console.log($(this).val().toLowerCase()); //
-    console.log($(this).val().toLowerCase().normalize('NFD').replace(/[^0-9a-zàáạảãâầấậẩẫăằắặẳẵèéẹẻẽêềếệểễìíịỉĩòóọỏõôồốộổỗơờớợởỡùúụủũưừứựửữỳýỵỷỹđ\s]/gi), '');
+    // console.log($(this).val().toLowerCase().normalize('NFD').replace(/[^0-9a-zàáạảãâầấậẩẫăằắặẳẵèéẹẻẽêềếệểễìíịỉĩòóọỏõôồốộổỗơờớợởỡùúụủũưừứựửữỳýỵỷỹđ\s]/gi), '');
+    let keyword = ChangeToSlug($(this).val().toLowerCase())
+
+    let sortCloneProduct = products.filter((product, i) => ChangeToSlug(product.name.toLowerCase()).includes(keyword))
+    console.log(keyword === '')
+    if(keyword === '') {
+        renderProduct(0, limit)
+    } else {
+        renderProduct(0, 0, 1, sortCloneProduct)
+    }
+
 });
