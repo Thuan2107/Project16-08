65e082f917a2165a6e85bbb0f15f26d6c65d0430
kodoku169 <79566948+kodoku169@users.noreply.github.com>
2022-08-16 17:39:58 +0700

    search product js

--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -98,5 +98,7 @@
         <script src="/js/product.js"></script>
         <script src="/js/menu_category.js"></script>
         <script src="/js/filter.js"></script>
+        <script src="/js/handle/sortPriceAndRate.js"></script>
+        <script src="/js/handle/pagination.js"></script>
     </body>
 </html>
