d7af690fc2015151f34c834e2f300bdab70fa623
Thuan2107 <mtnguyen2107@gmail.com>
2022-08-22 11:53:10 +0700

    update price

--- blog/public/js/payment.js
+++ blog/public/js/payment.js
@@ -1,15 +1,10 @@
 
-
 $( document ).ready(function() {
-    // console.log()
     let list_payment = []
     if(getCookie('cart_box')){
         list_payment = JSON.parse(getCookie('cart_box'))
     }
-    for (let i = 0; i < list_payment.length; i++) {
-        // console.log(list_payment[1])
-    }
-    console.log(typeof list_payment)
+
     let tr = ''
     list_payment.map((item,index) => {
 
@@ -35,18 +30,8 @@ $( document ).ready(function() {
         })
 
     $("#table_payments").html(tr)
-    // console.log(222)
-
 
     getTotal()
-    // function getTotal(){
-    //     let summ=0;
-    //     for (let i = 0; i < list_payment.length; i++) {
-    //         summ += parseInt(list_payment[i].cost_price)
-    //     }
-    //     console.log(summ)
-    //     $('#total').text(summ.toLocaleString('en-US'))
-    // }
 
     function getTotal(){
         let tr_price = $('.title_one')
@@ -56,9 +41,22 @@ $( document ).ready(function() {
         })
         $("#total").text(total.toLocaleString('en-US'))
 
+
     }
 
+    // function getTotal(){
+    //     let summ=0;
+    //     for (let i = 0; i < list_payment.length; i++) {
+    //         let total = list_payment[i].cost_price * list_payment[i].quantity
+    //         summ += total
+    //
+    //     }
+    //     return summ.toLocaleString('en-US')
+    // }
 
+    function formatNum(num){
+        return num.replaceAll(',','')
+    }
 
     $(".delete_btn").on('click', function () {
         let id = $(this).data('id');
@@ -70,38 +68,90 @@ $( document ).ready(function() {
             }
         }
         setCookie('cart_box', JSON.stringify(newCookie))
-        /**
-         * Chuyen tu json sang array
-         * tim phan tu co id bang id vua chon va xoa phan tu ra khoi mang
-         * chuyen arr sang json
-         * luu lai cookie
-         */
+
         getTotal()
-        // $('#total').text(getTotal())
+        console.log(getTotal())
+
     })
 
-    $("#table_payments").html(tr)
-    function getTotal(){
-        let summ=0;
-        for (let i = 0; i < list_payment.length; i++) {
-            let total = list_payment[i].cost_price * list_payment[i].quantity
-            summ += total
-
-        }
-        return summ.toLocaleString('en-US')
-    }
-
-    console.log(getTotal())
-    $('#total').text(getTotal())
 
 
 });
 
-
-
-
-
-
-
-
-
+// $( document ).ready(function() {
+//     let list_payment = []
+//     if(getCookie('cart_box')){
+//         list_payment = JSON.parse(getCookie('cart_box'))
+//     }
+//
+//     let tr = ''
+//     list_payment.map((item,index) => {
+//
+//         tr+= `
+//                 <tr class="title_one" >
+//                             <td>${index + 1}</td>
+//                             <td>${item.name}</td>
+//                             <td>${item.category_name}</td>
+//                             <td>${item.inventory}</td>
+//                             <td class="details_pay">
+//                                 <input type="text" class="input_number" value="${item.quantity}" />
+//                             </td>
+//                             <td >${(item.cost_price * 1).toLocaleString('en-US')}</td>
+//
+//
+//                             <td class="td_price">${(item.quantity * item.cost_price).toLocaleString('en-US')}</td>
+//
+//                             <td>
+//                                 <i class="fa-solid fa-trash-can delete_btn" data-id=${item.id}></i>
+//                             </td>
+//                         </tr>
+//            `
+//     })
+//
+//     $("#table_payments").html(tr)
+//
+//
+//     getTotal()
+//     // function getTotal(){
+//     //     let summ=0;
+//     //     for (let i = 0; i < list_payment.length; i++) {
+//     //         summ += parseInt(list_payment[i].cost_price)
+//     //     }
+//     //     console.log(summ)
+//     //     $('#total').text(summ.toLocaleString('en-US'))
+//     // }
+//
+//     function getTotal(){
+//         let tr_price = $('.title_one')
+//         let total = 0
+//         tr_price.each(function (){
+//             total += Number(formatNum($(this).find('.td_price').text()))
+//         })
+//         $("#total").text(total.toLocaleString('en-US'))
+//
+//     }
+//
+//     function formatNum(num){
+//         return num.replaceAll(',','')
+//     }
+//
+//
+//     $(".delete_btn").on('click', function () {
+//         let id = $(this).data('id');
+//         $(this).parents('tr').remove();
+//         let newCookie = JSON.parse(getCookie('cart_box'));
+//         for (let i = 0; i < newCookie.length; i++) {
+//             if (newCookie[i].id == id){
+//                 newCookie.splice(i, 1)
+//             }
+//         }
+//         setCookie('cart_box', JSON.stringify(newCookie))
+//         /**
+//          * Chuyen tu json sang array
+//          * tim phan tu co id bang id vua chon va xoa phan tu ra khoi mang
+//          * chuyen arr sang json
+//          * luu lai cookie
+//          */
+//         getTotal()
+//     })
+// });
